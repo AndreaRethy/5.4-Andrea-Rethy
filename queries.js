df24af47771b6ce7718edb40dc06154ff87ec99c
Andrea Rethy <125359387+AndreaRethy@users.noreply.github.com>
2024-08-22 11:02:32 +0200

    Query 20

--- queries.js
+++ queries.js
@@ -291,25 +291,44 @@ const restaurants = db.get('Restaurants');
 
 
   /* 19. Write a query to find the restaurant_id, name, borough, and cuisine for those restaurants that are NOT in Staten Island, Queens, Bronx, or Brooklyn. */
-  restaurants.find({
-    "borough": { $nin: [/Staten Island/i, /Queens/i, /Bronx/i, /Brooklyn/i] }
-  }, { fields: { 
-      restaurant_id: 1, 
-      name: 1, 
-      borough: 1, 
-      cuisine: 1 } })
-    .then(docs => {
-      console.log(docs);
-    })
-    .catch(err => {
-      console.error(err);
-    })
-    .finally(() => {
-      db.close();
-    });
+  // restaurants.find({
+  //   "borough": { $nin: [/Staten Island/i, /Queens/i, /Bronx/i, /Brooklyn/i] }
+  // }, { fields: { 
+  //     restaurant_id: 1, 
+  //     name: 1, 
+  //     borough: 1, 
+  //     cuisine: 1 } })
+  //   .then(docs => {
+  //     console.log(docs);
+  //   })
+  //   .catch(err => {
+  //     console.error(err);
+  //   })
+  //   .finally(() => {
+  //     db.close();
+  //   });
+
+
+/* 20. Write a query to find the restaurant_id, name, borough, and cuisine for those restaurants that score less than 10. */
+restaurants.find({
+  "grades": { $elemMatch: { "score": { $lt: 10 } } }
+}, { fields: { 
+    restaurant_id: 1, 
+    name: 1, 
+    borough: 1, 
+    cuisine: 1 } })
+  .then(docs => {
+    console.log(docs);
+  })
+  .catch(err => {
+    console.error(err);
+  })
+  .finally(() => {
+    db.close();
+  });
+
 
 /*
-20. Escriu una consulta per trobar el restaurant_id, name, borough i cuisine per a aquells restaurants que aconsegueixin una nota menor que 10.
 21. Escriu una consulta per trobar el restaurant_id, name, borough i cuisine per a aquells restaurants que preparen marisc ('seafood') excepte si són 'American ', 'Chinese' o el name del restaurant comença amb lletres 'Wil'.
 22. Escriu una consulta per trobar el restaurant_id, name i grades per a aquells restaurants que aconsegueixin un grade de "A" i un score d'11 amb un ISODate "2014-08-11T00:00:00Z".
 23. Escriu una consulta per trobar el restaurant_id, name i grades per a aquells restaurants on el 2n element de l'array de graus conté un grade de "A" i un score 9 amb un ISODate "2014-08-11T00:00:00Z".
